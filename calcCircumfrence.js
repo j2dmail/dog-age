d79437137ad2898b9acade56346355b7c5a06bcc
Yinka Jimoh <111042679+j2dmail@users.noreply.github.com>
2023-02-17 12:22:37 -0800

    circumference calculation

---
+++ calcCircumfrence.js
@@ -0,0 +1,8 @@
+function calcCircumfrence(radius){
+    let result = (22/7) * 2* radius; 
+     return result;
+   }
+   
+   console.log(calcCircumfrence(7));
+   console.log(calcCircumfrence(21));
+   console.log(calcCircumfrence(14));
