99f41b0d39ffa9c1f951e8b2abccce55fa7015a6
Yinka Jimoh <111042679+j2dmail@users.noreply.github.com>
2023-02-17 10:11:52 -0800

    dog age

---
+++ dogage.js
@@ -0,0 +1,7 @@
+function calculateDogAge(dogsAge){
+  return "Your doggie is " + dogsAge/7 + " years old in dog years! "
+}
+
+console.log(calculateDogAge(21));
+console.log(calculateDogAge(21));
+console.log(calculateDogAge(21));
